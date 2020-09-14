c324ffb2f9443d0bb62b057dd25415feded5512e
EunsangJeon <eunsang.jeon@samsung.com>
2020-09-14 11:38:16 +0900

    working on GoogleAuth

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,11 +1,23 @@
 import express, { Application } from 'express';
 import passport from 'passport';
 import { Strategy as googleStrategy } from 'passport-google-oauth20';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from './config/keys';
+import { PORT } from './config/variables';
 
 const app: Application = express();
-const PORT: any = process.env.PORT;
 
-passport.use(new googleStrategy());
+passport.use(
+  new googleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: 'auth/google/callback',
+    },
+    (accessToken: string) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}.`);
