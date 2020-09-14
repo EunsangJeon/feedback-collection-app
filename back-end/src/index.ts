e921537feeaad14a85ef3f6d04bd7cc0324a9028
EunsangJeon <eunsang.jeon@samsung.com>
2020-09-14 10:33:09 +0900

    remove env file

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,9 +1,11 @@
 import express, { Application } from 'express';
 import passport from 'passport';
-import googlStrategy from 'passport-google-oauth20';
+import { Strategy as googleStrategy } from 'passport-google-oauth20';
 
 const app: Application = express();
 const PORT: any = process.env.PORT;
+
+passport.use(new googleStrategy());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}.`);
