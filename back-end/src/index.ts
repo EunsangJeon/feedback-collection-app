23920d3f3b017ec320d0fa3824f081d5b07fbf72
EunsangJeon <eunsang.jeon@samsung.com>
2020-09-14 14:39:47 +0900

    working on oAuth

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,6 +1,10 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import passport from 'passport';
-import { Strategy as googleStrategy } from 'passport-google-oauth20';
+import {
+  Profile,
+  Strategy as googleStrategy,
+  VerifyCallback,
+} from 'passport-google-oauth20';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from './config/keys';
 import { PORT } from './config/variables';
 
@@ -11,13 +15,31 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: 'auth/google/callback',
+      callbackURL: '/auth/google/callback',
     },
-    (accessToken: string) => {
-      console.log(accessToken);
+    (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback
+    ) => {
+      console.log(`accessToken: ${accessToken}`);
     }
   )
 );
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({ message: 'service on' });
+});
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}.`);
