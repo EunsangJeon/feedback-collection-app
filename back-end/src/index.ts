6ad2dbe8344a4757df93b13c33afcac286980561
EunsangJeon <eunsang.jeon@samsung.com>
2020-09-15 13:19:29 +0900

    modified callback

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import passport from 'passport';
 import {
   Profile,
-  Strategy as googleStrategy,
+  Strategy as GoogleStrategy,
   VerifyCallback,
 } from 'passport-google-oauth20';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from './config/keys';
@@ -10,23 +10,23 @@ import { PORT } from './config/variables';
 
 const app: Application = express();
 
-passport.use(
-  new googleStrategy(
-    {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
-    },
-    (
-      accessToken: string,
-      refreshToken: string,
-      profile: Profile,
-      done: VerifyCallback
-    ) => {
-      console.log(`accessToken: ${accessToken}`);
-    }
-  )
+const googleStrategy = new GoogleStrategy(
+  {
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback',
+  },
+  (
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: VerifyCallback
+  ) => {
+    console.log(`accessToken: ${accessToken}`);
+  }
 );
+
+passport.use(googleStrategy);
 
 app.get('/', (req: Request, res: Response) => {
   res.send({ message: 'service on' });
