b7f0cca71d82cb3a77fe4ece8806f34b5ec4c320
Eunsang <nicolaus1207@gmail.com>
2020-09-20 21:35:27 +0900

    jwt dead end

--- back-end/src/config/passport.ts
+++ back-end/src/config/passport.ts
@@ -1,19 +1,52 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as JwtStrategy } from 'passport-jwt';
+import { Document } from 'mongoose';
 import googleStrategyVerify from '../services/googleStrategyVerify';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from './keys';
+import {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  COOKIE_SESSION_KEY,
+} from './keys';
+import User from '../models/userModel';
 
-passport.serializeUser((user, done) => {
-  console.log(`passport serialized: ${user}`);
-  // TODO
-  done(null, user);
+passport.serializeUser<Document, string>((user, done) => {
+  console.log(`passport serialized user with id: ${user.id}`);
+  done(null, user.id);
 });
 
-passport.deserializeUser((obj, done) => {
-  console.log(`passport deserialized: ${obj}`);
-  // TODOs
-  done(null, obj);
+passport.deserializeUser<Document, string>((id, done) => {
+  console.log(`passport deserialized with id: ${id}`);
+  User.findById(id)
+    .then((user) => {
+      if (user) {
+        done(null, user);
+      } else {
+        done(new Error('passport deserializeUser could not get user'));
+      }
+    })
+    .catch((err) => {
+      done(err);
+    });
 });
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: (req) => {
+        let token;
+        if (req && req.cookies) {
+          token = req.cookies.jwt;
+        }
+        return token;
+      },
+      secretOrKey: COOKIE_SESSION_KEY,
+    },
+    (payload, done) => {
+      console.log('jwt auth was called');
+    }
+  )
+);
 
 passport.use(
   new GoogleStrategy(
--- back-end/src/routes/apiRoutes.ts
+++ back-end/src/routes/apiRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import passport from 'passport';
 
 const router = Router();
 
-router.get('/current-user', (req, res) => {
-  console.log(req.user);
-  res.send(req.user);
-});
+router.get(
+  '/current-user',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    console.log(req.user);
+    res.send(req.user);
+  }
+);
 
 export default router;
