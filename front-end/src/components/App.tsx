2b1c0f403f824322dd6c0a87c5005d093f74f093
Eunsang <nicolaus1207@gmail.com>
2020-09-27 11:58:08 +0900

    initial front-end routes

--- front-end/src/components/App.tsx
+++ front-end/src/components/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 function App(): JSX.Element {
   return (
     <div>
-      <p>React App</p>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
